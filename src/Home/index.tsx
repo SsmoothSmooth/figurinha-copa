c39c69011fba10463943ea9354699b6744ac2f6b
José Wilson de Lima <josewilson791@gmail.com>
2022-11-09 09:57:25 -0300

    initializing user front camera

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Camera, CameraType } from 'expo-camera';
 import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
 
 import { Header } from '../components/Header';
@@ -9,7 +10,13 @@ import { styles } from './styles';
 import { POSITIONS, PositionProps } from '../utils/positions';
 
 export function Home() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
   const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+
+  useEffect(() => {
+    Camera.requestCameraPermissionsAsync()
+    .then(response => setHasCameraPermission(response.granted))
+  },[])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,7 +26,14 @@ export function Home() {
 
           <View style={styles.picture}>
 
-            <Image source={{ uri: 'https://github.com/SsmoothSmooth.png' }} style={styles.camera} />
+            {
+              !hasCameraPermission ? 
+              <Camera 
+                style={styles.camera}
+                type={CameraType.front}
+              /> : 
+              <Image source={{ uri: 'https://filestore.community.support.microsoft.com/api/images/490b996b-e45f-4985-b2af-cf36da33849a?upload=true' }} style={styles.camera} />
+            }
 
             <View style={styles.player}>
               <TextInput
